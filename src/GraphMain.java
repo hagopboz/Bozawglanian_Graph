78fe34bc2a88e1e34b9f0932b1e81a3ef0a1f854
bozawh <bozawh@AXTEMP16.division.daviswire.com>
2015-05-22 12:11:25 -0700

    Menu

--- src/GraphMain.java
+++ src/GraphMain.java
@@ -1,4 +1,61 @@
+import java.util.*;
 
 public class GraphMain {
-
+	private static Scanner sc = new Scanner(System.in);
+	
+	
+	public static void displayMenu() {	
+		System.out.println(" Q   Query the city information by entering the city code.");
+		System.out.println(" D   Find the minimum distance between two cities.");
+		System.out.println(" I   Insert a road by entering two city codes and distance.");
+		System.out.println(" R   Remove an existing road by entering two city codes.");
+		System.out.println(" H   Display this message.");
+		System.out.println(" E   Exit.");
+		command();
+	}
+	
+	public static void command() {
+		System.out.print("Command? ");
+		char menuInput = sc.next().toLowerCase().charAt(0);
+		if(menuInput == 'q' || menuInput == 'd' || menuInput == 'i'|| menuInput == 'r' || menuInput == 'e'){
+			int valueInput = sc.nextInt();
+			moveFromMenu(menuInput, valueInput);
+		} else if(menuInput == 'h') {
+			displayMenu();
+			command();
+		} else {
+			System.out.println("Invalid command.");
+			System.out.println("Please only enter one of the follow characters.");
+			sc.nextLine();
+			command();
+		} 
+	}
+	
+	public static void moveFromMenu(char option, int value) {		
+		boolean run = true;
+		while(run){
+			switch(option){
+			case 'i':
+				System.out.println();
+				command();
+				break;
+			case 'd':
+				System.out.println();
+				command();
+				break;
+			case 'p':
+				System.out.println();
+				command();
+				break;
+			case 's':
+				System.out.println();
+				command();
+			case 'e':
+				System.out.println("Thank you for using!");
+				run = false;
+				System.exit(0);
+				break;
+			}
+		}
+	}
 }
